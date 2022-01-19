046d2bfc6fcc9e4990381a1c0b36b976729c4c69
kotobuki <fkjtmy@gmail.com>
2022-01-19 13:15:44 +0900

    検索ボックス実装

--- src/laravel/resources/views/main/index.blade.php
+++ src/laravel/resources/views/main/index.blade.php
@@ -9,6 +9,37 @@
 </head>
 <body>
     <header>
+        <div class="header-img">
+            <img src="images/icon.jpg" alt="">
+        </div>
+        {{-- <div class="header-a">
+            <a class="letter">ログイン</a>
+            <p> | </p>
+            <a class="letter">ユーザー登録</a>
+            <p> | </p>
+            <a class="letter">お問い合わせ</a>
+        </div> --}}
+        @if (Route::has('login'))
+                <div class="header-a hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <ul class="nav">
+                        @auth
+                        <li><a href="{{ url('/dashboard') }}" class="letter text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a></li>
+                        @else
+                        <li><a href="{{ route('login') }}" class="letter text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a></li>
+
+                            @if (Route::has('register'))
+                            <li><a href="{{ route('register') }}" class="letter ml-4 text-sm text-gray-700 dark:text-gray-500 underline">ユーザー登録</a></li>
+                            @endif
+                        @endauth
+                        <li><a href="https://www.google.com" class="letter underline">お問合せ</a></li>
+                        <li><a href="https://www.google.com" class="letter underline">つくりえとは？</a></li>
+                    </ul>
+
+                    <div id="hamburger">
+                        <span></span>
+                    </div>
+                </div>
+            @endif
     </header>
     <div class="content">
         <section class="top">
@@ -16,11 +47,26 @@
                 <img class="top-image-pc" src="images/top_img.jpg" alt="">
             </div>
             <div class="top-p">
-                <p class="head">クリエイターの<br>つくえを共有しよう<p>
+                <p class="head letter">クリエイターの<br>つくえを共有しよう<p>
             </div>
         </section>
-
     </div>
+    <div class="search-box-bg">
+        <div class="search-box">
+            <form>
+                @csrf
+                <input class="search-text-box" type="text">
+                <select class="search-select-box" name="example">
+                    <option hidden>カテゴリ</option>
+                    <option value="music">音楽</option>
+                    <option value="movie">動画</option>
+                    <option value="illustration">イラスト</option>
+                    </select>
+                <input class="search-select-submit" type="submit" value="検索">
+            </form>
+        </div>
+    </div>
+
 
 
 
