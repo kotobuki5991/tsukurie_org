aae7a83b0290195ced90eb8fc1f349710de92304
kotobuki <fkjtmy@gmail.com>
2022-01-25 01:06:46 +0900

    ログイン後のリダイレクト先変更

--- src/laravel/public/js/common.js
+++ src/laravel/public/js/common.js
@@ -1,7 +1,7 @@
 'use strict';
 
 ScrollReveal().reveal('.float', {
-    duration: 500, // アニメーションの完了にかかる時間
+    duration: 300, // アニメーションの完了にかかる時間
     viewFactor: 0.2, // 0~1,どれくらい見えたら実行するか
     reset: false,   // 何回もアニメーション表示するか
     distance: '30px',
