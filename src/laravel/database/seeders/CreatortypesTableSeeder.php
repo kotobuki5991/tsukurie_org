f606a1115729f57d5a9e87def4f574c8ce54e9e5
kotobuki <fkjtmy@gmail.com>
2022-01-27 16:15:33 +0900

    mysqlの文字化け直す

---
+++ src/laravel/database/seeders/CreatortypesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+class CreatortypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $type1 = [
+            'creatortype' => '音楽',
+        ];
+        $type2 = [
+            'creatortype' => 'イラスト',
+        ];
+        $type3 = [
+            'creatortype' => '動画',
+        ];
+
+        $types = [$type1, $type2, $type3];
+
+        DB::table('creatortypes')->insert($types);
+
+    }
+}
