2a775975bb3a1c0a48fdde30eee79e258474f0d3
kotobuki <fkjtmy@gmail.com>
2022-01-31 21:44:09 +0900

    トップページでの全件検索実装

--- src/laravel/app/Http/Controllers/MainController.php
+++ src/laravel/app/Http/Controllers/MainController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\Storage;
 
 class MainController extends Controller
 {
     public function index(Request $request)
     {
-        return view('main/index');
+        $user_profiles = Profile::where('publish_flag', 1)->paginate(1);
+        // dd($user_profiles);
+        $profile_ary = [];
+        foreach($user_profiles as $user_profile){
+            $profile_ary[] = [
+                'user_id' => $user_profile->user_id,
+                'profile_name' => $user_profile->profile_name,
+                'creator_type' => $user_profile->creator_type->creator_type, //creator_typesのidとprofilesのcreator_types_idで紐づいたcreator_typeを取得
+                'top_image' => isset($user_profile->top_image) ? Storage::disk('s3')->url($user_profile->top_image) : null, //s3のurl
+                'profile_icon' => isset($user_profile->profile_icon) ? Storage::disk('s3')->url($user_profile->profile_icon) : null, //s3のurl
+                'message' => $user_profile->message,
+            ];
+        }
+
+        return view('main/index', ['profile_ary' => $profile_ary, 'paginate' =>  $user_profiles ]);
     }
 
     // 投稿ページへの遷移
--- src/laravel/resources/views/main/index.blade.php
+++ src/laravel/resources/views/main/index.blade.php
@@ -11,9 +11,9 @@
 @section('search_box')
 <div class="search-box-bg">
     <div class="search-box">
-        <form>
+        <form action="" method="post">
             @csrf
-            <input class="search-text-box" type="text">
+            <input class="search-text-box" type="text" name="search-word">
             <select class="search-select-box" name="example">
                 <option hidden>カテゴリ</option>
                 <option value="music">音楽</option>
@@ -29,6 +29,41 @@
 {{-- メインコンテンツ --}}
 @section('main_contents')
 <div class="show-cards-area">
+
+    @foreach ($profile_ary as $profile)
+    <div class="desk-card float">
+        @switch($profile["creator_type"])
+            @case('音楽')
+            <div class="category-tag-music mouse-hover-transparent"><a href="https://www.google.com"><h4>{{ $profile["creator_type"] }}</h4></a></div>
+                @break
+            @case('イラスト')
+            <div class="category-tag-illust mouse-hover-transparent"><a href="https://www.google.com"><h4>{{ $profile["creator_type"] }}</h4></a></div>
+                @break
+            @case('動画')
+            <div class="category-tag-movie mouse-hover-transparent"><a href="https://www.google.com"><h4>{{ $profile["creator_type"] }}</h4></a></div>
+                @break
+            @default
+            {{-- タスク 未選択タグを準備 --}}
+        @endswitch
+        {{-- <div class="category-tag-music mouse-hover-transparent"><a href="https://www.google.com"><h4>{{ $profile["creator_type"] }}</h4></a></div> --}}
+        <div class="desk-card-image-area">
+            <a href="/show_post">
+                <img class="desk-card-image mouse-hover-expansion" src="{{ $profile["top_image"] ?: asset('/uploaded_images/1.jpg')  }}" alt="">
+            </a>
+        </div>
+        {{-- 後で遷移先変える --}}
+        <div>
+            <a href="https://www.google.com">
+                <img class="desk-card-user-icon" src="{{ $profile["profile_icon"] ?: asset('/images/default_user_icon.jpeg') }}" alt="">
+            </a>
+        </div>
+        <div>
+            <div class="user-name"><h3>{{ $profile["profile_name"] }}</h3></div>
+            <div class="user-message letter"><h5>{!! nl2br(e($profile["message"])) !!}</h5></div>
+        </div>
+    </div>
+    @endforeach
+{{--
     <div class="desk-card float">
         <div class="category-tag-music mouse-hover-transparent"><a href="https://www.google.com"><h4>音楽</h4></a></div>
         <div class="desk-card-image-area"><a href="/show_post"><img class="desk-card-image mouse-hover-expansion" src="{{ asset('uploaded_images/fukuju.jpg') }}" alt=""></a></div>
@@ -136,9 +171,12 @@
             <div class="user-name"><h3>kotobuki</h3></div>
             <div class="user-message letter"><h5>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbb</h5></div>
         </div>
-    </div>
+    </div> --}}
 </div>
 
-<p style="align-items: center"> <  1  2  3  4  5  6  ></p>
+{{-- <p style="align-items: center"> <  1  2  3  4  5  6  ></p> --}}
+<div class="pagenate">
+    {{ $paginate->links() }}
+</div>
 
 @endsection
